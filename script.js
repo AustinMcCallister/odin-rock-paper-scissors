f4fae93a3d4f6a25a3d346ca623dfb9433841133
Austin McCallister <austin.mccallister@protonmail.com>
2022-03-03 19:03:34 -0500

    Add playRound function

--- script.js
+++ script.js
@@ -10,3 +10,33 @@ function computerPlay() {
         return 'Scissors';
     }
 }
+
+function playRound(playerSelection, computerSelection) {
+    if (playerSelection.toLowerCase() === computerSelection.toLowerCase()) {
+        return 'It\'s a tie!';
+    }
+    else if (playerSelection.toLowerCase() === 'rock') {
+        if (computerSelection.toLowerCase() === 'paper') {
+            return 'You Lose! Paper beats Rock!';
+        }
+        else if (computerSelection.toLowerCase() === 'scissors') {
+            return 'You Win! Rock beats Scissors!'
+        }
+    }
+    else if (playerSelection.toLowerCase() === 'paper') {
+        if (computerSelection.toLowerCase() === 'scissors') {
+            return 'You Lose! Scissors beats Paper!';
+        }
+        else if (computerSelection.toLowerCase() === 'rock') {
+            return 'You Win! Paper beats Rock!'
+        }
+    }
+    else if (playerSelection.toLowerCase() === 'scissors') {
+        if (computerSelection.toLowerCase() === 'rock') {
+            return 'You Lose! Rock beats Scissors!';
+        }
+        else if (computerSelection.toLowerCase() === 'paper') {
+            return 'You Win! Scissors beats Paper!'
+        }
+    }
+}
