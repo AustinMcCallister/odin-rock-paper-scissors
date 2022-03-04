28b4bfbc2ff441212b05699b5cc277580c7fde6f
Austin McCallister <austin.mccallister@protonmail.com>
2022-03-03 20:04:46 -0500

    Convert playRound return statements into integers

--- script.js
+++ script.js
@@ -13,30 +13,30 @@ function computerPlay() {
 
 function playRound(playerSelection, computerSelection) {
     if (playerSelection.toLowerCase() === computerSelection.toLowerCase()) {
-        return 'It\'s a tie!';
+        return 2;
     }
     else if (playerSelection.toLowerCase() === 'rock') {
         if (computerSelection.toLowerCase() === 'paper') {
-            return 'You Lose! Paper beats Rock!';
+            return 0;
         }
         else if (computerSelection.toLowerCase() === 'scissors') {
-            return 'You Win! Rock beats Scissors!'
+            return 1;
         }
     }
     else if (playerSelection.toLowerCase() === 'paper') {
         if (computerSelection.toLowerCase() === 'scissors') {
-            return 'You Lose! Scissors beats Paper!';
+            return 0;
         }
         else if (computerSelection.toLowerCase() === 'rock') {
-            return 'You Win! Paper beats Rock!'
+            return 1;
         }
     }
     else if (playerSelection.toLowerCase() === 'scissors') {
         if (computerSelection.toLowerCase() === 'rock') {
-            return 'You Lose! Rock beats Scissors!';
+            return 0;
         }
         else if (computerSelection.toLowerCase() === 'paper') {
-            return 'You Win! Scissors beats Paper!'
+            return 1;
         }
     }
 }
